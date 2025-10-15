7672af4d0fbb5073473492307f919444c784d531
Mohammad Shan <mohdshan1024@gmail.com>
2025-10-15 17:15:56 +0530

    progress little bit

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,35 @@
+import Usercards from "@/components/Usercards"
+import Countchart from "@/components/Countchart"
 
 const page = () => {
   return (
-    <div>Admin page</div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left side */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8 ">
+        {/* User Cards */}
+        <div className="flex gap-4 justify-between flex-wrap ">
+          <Usercards type="student" />
+          <Usercards type="teacher" />
+          <Usercards type="staff" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <Countchart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+
+          </div>
+        </div>
+      </div>
+
+
+      {/* Right side */}
+      <div className="w-full lg:w-1/3"></div>
+
+    </div>
   )
 }
 
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
-import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -9,23 +9,25 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
+    <div className="h-screen w-screen flex overflow-hidden">
       {/* LEFT */}
-      <div className="w-[25%] md:w-[8%] lg:w-[16%] xl:w-[25%] p-4">
+      <aside className="w-[25%] md:w-[8%] lg:w-[16%] xl:w-[25%] p-4 overflow-y-auto border-r scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent hover:scrollbar-thumb-gray-400">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <Image src="/logo_02.jpg" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">BHN ENGLISH HR SEC SCHOOL</span>
         </Link>
         <Menu />
-      </div>
+      </aside>
       {/* RIGHT */}
-      <div className="w-[75%] md:w-[92%] lg:w-[84%] xl:w-[75%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-        <Navbar />
-        {children}
-      </div>
+      <main className="flex-1 bg-[#F7F8FA] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 hover:scrollbar-thumb-gray-400">
+        <div className="flex flex-col h-full">
+          <Navbar />
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
